7924782a72a9297c1e1fc8e72f236d8ec0b4ae9f
RACHNA PANDITA <rachna.pandita@db.com>
2021-10-15 10:33:08 +0530

    added html code with some functions

---
+++ main.js
@@ -0,0 +1,14 @@
+const numberButtons = document.querySelectorAll('number');
+const operationButtons = document.querySelectorAll('operation')
+const equalsButton = document.getElementById('equal-button');
+// const deleteButton = document.getElementById('del-button');
+const allClearButton = document.getElementById('all-clear')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+    })
+})
+ 
